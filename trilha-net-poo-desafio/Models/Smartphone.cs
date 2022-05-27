3075128157eb3dabda681da4d051482925bd6841
Liandro dos Anjos <oliverliandro@gmail.com>
2022-05-26 22:01:39 -0300

    Desafio poo

---
+++ trilha-net-poo-desafio/Models/Smartphone.cs
@@ -0,0 +1,30 @@
+namespace DesafioPOO.Models
+{
+    public abstract class Smartphone
+    {
+        public string Numero { get; set; }
+        public string  Modelo { get; set; }
+        public string IMEI { get; set; }
+        public int Memoria { get; set; }
+
+        protected Smartphone(string numero, string modelo, string iMEI, int memoria)
+        {
+            Numero = numero;
+            Modelo = modelo;
+            IMEI = iMEI;
+            Memoria = memoria;
+        }
+
+        public void Ligar()
+        {
+            Console.WriteLine("Ligando...");
+        }
+
+        public void ReceberLigacao()
+        {
+            Console.WriteLine("Recebendo ligação...");
+        }
+
+        public abstract void InstalarAplicativo(string nomeApp);
+    }
+}
---
+++ trilha-net-poo-desafio/Program.cs
@@ -0,0 +1,15 @@
+﻿using DesafioPOO.Models;
+
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia("98745666", "xpto", "vkvna4564", 128);
+nokia.Ligar();
+nokia.InstalarAplicativo("Uber");
+
+Console.WriteLine("\n");
+
+
+Console.WriteLine("Smartphone Iphone:");
+Smartphone iphone = new Iphone("65421225", "007", "KHHUGGY455", 32);
+iphone.Ligar();
+iphone.InstalarAplicativo("Zafari");
+
